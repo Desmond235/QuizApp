f58787531ad0ae0f73ceaf0599fb0d6ccf997fcf
Adabe Desmond <adabedesmond@gmail.com>
2023-07-03 21:51:53 +0000

    modified file

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -6,20 +6,21 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: double.infinity,
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('You answered x out of y questions correctly!'),
-              const SizedBox(height: 30),
-              TextButton(
-                onPressed: () {},
-                child: const Text('Restart quiz'),
-              ),
-            ],
-          ),
-        ));
+      width: double.infinity,
+      child: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answered x out of y questions correctly!'),
+            const SizedBox(height: 30),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
