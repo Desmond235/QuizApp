013d529cf3130a4e2ae46b9b824061fd2c2547c1
Adabe Desmond <adabedesmond@gmail.com>
2023-07-17 18:24:59 +0000

    modified code

--- lib/questions_data.dart
+++ lib/questions_data.dart
@@ -1,37 +1,53 @@
 import 'package:flutter/material.dart';
 
 class QuestionsData extends StatelessWidget {
-  const QuestionsData({super.key, required this.summaryData});
+  const QuestionsData({super.key, required this.summaryData,required this.correctQuestion});
    
    final Color color = const Color.fromARGB(220, 255, 255, 255);
   final List<Map<String, Object>> summaryData;
+  final  bool correctQuestion;
+
   Widget text(String text, Color color) {
-    return Text(text,style: TextStyle(color: color));
+    return Text(text,style: TextStyle(color: color,));
   }
 
+ Widget corrextText(Widget child ) {
+    return SizedBox(child: child);
+  }
   @override
   Widget build(context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            text(
-              ((data['question_index'] as int) + 1).toString(),
-              color
-            ),
-            Column(
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
               children: [
-                text(data['question'].toString(),color),
-                const SizedBox(
-                  height: 5,
-                ),
-                text(data['user_answer'].toString(),color),
-                text(data['correct_answer'].toString(),color),
+                   CircleAvatar(
+                    child: Text(((data['question_index']as int) + 1).toString(),
+                     style:  TextStyle(
+                      color: color,
+                      backgroundColor:  correctQuestion ? Colors.blue : Colors.red
+                     ),
+                    ) ,
+                  ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      text(data['question'].toString(),color),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      text(data['user_answer'].toString(),color),
+                      text(data['correct_answer'].toString(),color),
+                    ],
+                  ),
+                )
               ],
-            )
-          ],
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -3,20 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_data.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
+  final List<String> chosenAnswers;
   const ResultScreen({super.key, required this.chosenAnswers});
 
-  final List<String> chosenAnswers;
 
-  List<Map<String, Object>> getData() {
-    List<Map<String, Object>> data = [];
-    for (var i = 0; i < chosenAnswers.length; i++) {
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+   List<Map<String, Object>> data = [];
+  List<Map<String, Object>> get getData {
+    for (var i = 0; i < widget.chosenAnswers.length; i++) {
       data.add(
         {
           'question_index': i,
           'question': questions[i].text,
           'correct_answer': questions[i].answer[0],
-          'user_answer': chosenAnswers[i]
+          'user_answer': widget.chosenAnswers[i]
         },
       );
     }
@@ -24,8 +29,36 @@ class ResultScreen extends StatelessWidget {
     return data;
   }
 
+  bool get getCorrectAnswer{
+     bool  correctAnswer = false;
+    data.where((element){
+        if(element['user_answer'] == element['correct_answer']){
+          setState(() {
+            correctAnswer = true;
+          });  
+      }
+      else{
+        setState(() {
+          correctAnswer = false;
+        });
+      }
+      return correctAnswer;
+    } 
+    );
+     return correctAnswer;
+  }
+
+  
   @override
   Widget build(BuildContext context) {
+    final summaryData = getData;
+    final numOfQuestions = questions.length;
+
+    // comparing number of  answered questions to number of total questions
+    final numOfCorrectAnswers = getData.where(
+      (element) => element['user_answer'] == element['correct_answer'],
+    );
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -34,7 +67,17 @@ class ResultScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-           QuestionsData(summaryData: getData()),
+            Text(
+              ' you answered ${numOfCorrectAnswers.length} out $numOfQuestions questions correctly',
+              style: const TextStyle(
+                color: Color.fromARGB(220, 255, 255, 255),
+              ),
+            ),
+            const SizedBox(height: 20),
+            QuestionsData(
+              summaryData: summaryData,
+              correctQuestion: getCorrectAnswer,
+            ),
             const SizedBox(height: 30),
             TextButton(
               onPressed: () {},
